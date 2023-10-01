e6db25b0a46f06b93b6cbea178e96004148072a4
Luis Brescia <luisfelipebrescia@gmail.com>
2023-10-01 17:06:34 -0300

    Adicionado esqueleto do front-end | Especificado responsáveis por cada processo na arquivo simplificado.md

---
+++ src/front/pages/exemplo.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- Use como se fosse um arquivo html normal -->
+</template>
+
+<style scoped>
+    /* Scoped faz com que o css seja aplicado apenas nesse componente */
+</style>
+
+<script setup>
+    // Setup faz com que não seja necessário usar o export default
+</script>

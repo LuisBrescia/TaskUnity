5d08b3dba6196f0ce637b7326095aa7db17e2f73
marcusvco <marcuscarvalhodeoliveira@gmail.com>
2023-10-22 13:05:40 -0300

    #43 - [Dev] Criação FormInput e começo tela criar equipe

---
+++ src/front/components/inputs/FormInput.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="input-group">
+        <label class="label"><slot /></label>
+        <input :name="name" :id="name" class="input" required="true"/>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: String,
+})
+</script>
+
+<style scoped>
+.input {
+    max-width: 190px;
+    height: 44px;
+    background-color: #181818;
+    border-radius: .5rem;
+    padding: 0 1rem;
+    border: 2px solid transparent;
+    font-size: 1rem;
+    transition: border-color .3s cubic-bezier(.25, .01, .25, 1) 0s, color .3s cubic-bezier(.25, .01, .25, 1) 0s, .2s cubic-bezier(.25, .01, .25, 1) 0s;
+}
+
+.label {
+    display: block;
+    margin-bottom: .3rem;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #D3D3D3;
+    transition: color .3s cubic-bezier(.25, .01, .25, 1) 0s;
+}
+
+.input:hover,
+.input:focus,
+.input-group:hover .input {
+    outline: none;
+    border-color: #ffffffff;
+}
+
+.input-group:hover .label,
+.input:focus {
+    color: #ffffffff;
+}
+</style>
---
+++ src/front/pages/create-team.vue
@@ -0,0 +1,21 @@
+<script setup>
+definePageMeta({
+    layout: 'dashboard'
+})
+</script>
+
+<template>
+    <main>
+        <form action="/">
+            <FormInput name="nome">Nome da Equipe</FormInput><br>
+            <FormInput name="descricao">Descrição</FormInput><br>
+            <WhiteButton>Criar Equipe</WhiteButton>
+        </form>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+}
+</style>

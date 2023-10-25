b1cad9454b7cb2b53ecef19dc277acb19af6024b
Andre Hyodo <dehyodo@gmail.com>
2023-10-24 21:58:55 -0300

    #47 [Front] Desenvolvimento da página contratar-freelancer.

---
+++ src/front/components/inputs/SelectInput.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="select-group">
+        <label class="label">{{ name }}</label>
+        <select name="" id="" class="select" required= "true"><slot /></select>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    name: String,
+})
+</script>
+
+<style scoped>
+.select {
+    max-width: 190px;
+    height: 44px;
+    background-color: #181818;
+    border-radius: .5rem;
+    padding: 0 1rem;
+    border: 2px solid transparent;
+    font-size: 1rem;
+    transition: border-color .3s cubic-bezier(.25, .01, .25, 1) 0s, color .3s cubic-bezier(.25, .01, .25, 1) 0s, .2s cubic-bezier(.25, .01, .25, 1) 0s;
+}
+
+.label {
+    display: block;
+    margin-bottom: .3rem;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #D3D3D3;
+    transition: color .3s cubic-bezier(.25, .01, .25, 1) 0s;
+}
+
+.select:hover,
+.select:focus,
+.select-group:hover .input {
+    outline: none;
+    border-color: #ffffffff;
+}
+
+.select-group:hover .label,
+.select:focus {
+    color: #ffffffff;
+}
+</style>
---
+++ src/front/pages/contratar-freelancer.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+    definePageMeta({
+        layout: 'dashboard'
+    })
+
+</script>
+
+<template>
+    <div class="flex mt-12 gap-12 buttom" >
+        <DefaultCard>
+            <div class="container mx-8 px-8 py-10 max-w-md">
+                <div class="grid gap-6">
+                
+                <div class="bg-white rounded-lg p-6 shadow-md">
+                    <SlideButton></SlideButton>
+                </div>
+
+                
+                <div class="bg-white rounded-lg p-6 shadow-md">
+                    <SlideButton></SlideButton>
+                </div>
+
+                
+                <div class="bg-white rounded-lg p-6 shadow-md">
+                    <SlideButton></SlideButton>
+                </div>
+
+                
+                <div class="bg-white rounded-lg p-6 shadow-md">
+                    <SlideButton></SlideButton>
+                </div>
+                </div>
+            </div>
+        </DefaultCard>
+        <DefaultCard>
+            <div class="flex mt-12 gap-12">
+                <FormInput name="nome">Nome do freelancer</FormInput><br>
+                <SelectInput name="Tempo de experiência">
+                    <option value="valor1">Até 1 ano</option>
+                    <option value="valor2" selected>De 1 a 3 anos</option>
+                    <option value="valor3">Mais de 3 anos</option>
+                </SelectInput>
+                <FormInput name="descricao">Tempo de experiência</FormInput><br>
+                <WhiteButton>Buscar</WhiteButton>
+
+            </div>
+            
+        </DefaultCard>
+    </div>
+
+</template>
+
+<style scoped>
+.buttom{
+    width: 100%;
+    height: 70%;
+}
+</style>

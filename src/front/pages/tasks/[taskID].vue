cf2b3588627bbcdecb1acd16ae842fc3ff5f17f7
LuisBrescia <luisfelipebrescia@gmail.com>
2023-10-26 12:58:33 -0300

    Adicionado página para tarefa específica

---
+++ src/front/pages/tasks/[taskID].vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex mt-12 gap-12">
+
+        <DefaultCard>
+
+
+            <header>Tarefa {{ taskID  }}</header>
+
+
+            <section>
+                <WhiteButton>Finzalizar Tarefa</WhiteButton>
+                <WhiteButton>Adiciovar comentário</WhiteButton>
+            </section>
+
+
+        </DefaultCard>
+
+    </div>
+</template>
+
+<script setup>
+
+const { taskID } = useRoute().params;
+/*
+> const dadosTarefa = await TaskUnityApi.get(`/tasks/${taskId}`);
+*/
+
+definePageMeta({
+    layout: 'dashboard'
+})
+
+</script>

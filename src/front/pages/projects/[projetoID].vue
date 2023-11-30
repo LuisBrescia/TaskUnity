a692744d0d05e0e2030828b6bd7c02752d2498cd
Gustavo Pereira de Oliveira <gpo0251588@gmail.com>
2023-11-30 00:31:17 -0300

    #60 [Sprint 4] Implementação da ligação entre front-end e back-end e navegabilidade do sistema para o processo Gerenciar Projetos

--- src/front/pages/projects/[projetoID].vue
+++ src/front/pages/projects/[projetoID].vue
@@ -1,27 +1,24 @@
 <template>
     <div class="flex mt-12 gap-12">
 
-        <DefaultCard>
+        <DefaultCard v-if="projeto">
 
             <header class="text-3xl font-semibold tracking-tighter">Dados do projeto</header>
 
             <main class="my-12">
                 <section class="mb-5">
                     <div class="text-sm font-thin text-neutral-500 tracking-widest">Nome</div>
-                    <div class="text-xl font-normal">Projeto 1</div>
+                    <div class="text-xl font-normal">{{ projeto.name }}</div>
                 </section>
 
                 <section class="mb-5">
                     <div class="text-sm font-thin text-neutral-500 tracking-widest">Data de início</div>
-                    <div class="text-xl font-normal">22/10/2023</div>
+                    <div class="text-xl font-normal">{{ projeto.startDate }}</div>
                 </section>
 
                 <section class="mb-5">
                     <div class="text-sm font-thin text-neutral-500 tracking-widest">Descrição</div>
-                    <div class="text-xl font-normal max-w-md">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
-                        et, quaerat corporis aliquam laborum vero earum incidunt hic praesentium?
-                    </div>
+                    <div class="text-xl font-normal max-w-md">{{ projeto.description }}</div>
                 </section>
             </main>
 
@@ -31,68 +28,81 @@
 
             <header class="text-3xl font-semibold tracking-tighter">Tarefas</header>
 
-            <main class="flex flex-col">        
+            <main class="flex flex-col">
                 <ul class="my-12">
-                <li class="flex justify-between items-center mb-5 gap-8">
-                    <div class="text-xl font-mono">Tarefa 1</div>
-                    <div class="flex gap-2">
-                        <BlueButton class="rounded-xl">
-                            <Icon name="material-symbols:edit" size="1.25rem"/>
-                        </BlueButton>
-                        <RedButton class="rounded-xl">
-                            <Icon name="material-symbols:delete" size="1.25rem"/>
-                        </RedButton>
-                    </div>
-                </li>
+                    <li class="flex justify-between items-center mb-5 gap-8">
+                        <div class="text-xl font-mono">Tarefa 1</div>
+                        <div class="flex gap-2">
+                            <BlueButton class="rounded-xl">
+                                <Icon name="material-symbols:edit" size="1.25rem" />
+                            </BlueButton>
+                            <RedButton class="rounded-xl">
+                                <Icon name="material-symbols:delete" size="1.25rem" />
+                            </RedButton>
+                        </div>
+                    </li>
 
-                <li class="flex justify-between items-center mb-5 gap-8">
-                    <div class="text-xl font-mono">Tarefa 2</div>
-                    <div class="flex gap-2">
-                        <BlueButton class="rounded-xl">
-                            <Icon name="material-symbols:edit" size="1.25rem"/>
-                        </BlueButton>
-                        <RedButton class="rounded-xl">
-                            <Icon name="material-symbols:delete" size="1.25rem"/>
-                        </RedButton>
-                    </div>
-                </li>
+                    <li class="flex justify-between items-center mb-5 gap-8">
+                        <div class="text-xl font-mono">Tarefa 2</div>
+                        <div class="flex gap-2">
+                            <BlueButton class="rounded-xl">
+                                <Icon name="material-symbols:edit" size="1.25rem" />
+                            </BlueButton>
+                            <RedButton class="rounded-xl">
+                                <Icon name="material-symbols:delete" size="1.25rem" />
+                            </RedButton>
+                        </div>
+                    </li>
 
-                <li class="flex justify-between items-center mb-5 gap-8">
-                    <div class="text-xl font-mono">Tarefa 3</div>
-                    <div class="flex gap-2">
-                        <BlueButton class="rounded-xl">
-                            <Icon name="material-symbols:edit" size="1.25rem"/>
-                        </BlueButton>
-                        <RedButton class="rounded-xl">
-                            <Icon name="material-symbols:delete" size="1.25rem"/>
-                        </RedButton>
-                    </div>
-                </li>
-            </ul>
-            
-            <WhiteButton>
-                <Icon name="carbon:task-add" size="1.5rem" class="mr-2" />
-                <span>Criar tarefa</span>
-            </WhiteButton>
+                    <li class="flex justify-between items-center mb-5 gap-8">
+                        <div class="text-xl font-mono">Tarefa 3</div>
+                        <div class="flex gap-2">
+                            <BlueButton class="rounded-xl">
+                                <Icon name="material-symbols:edit" size="1.25rem" />
+                            </BlueButton>
+                            <RedButton class="rounded-xl">
+                                <Icon name="material-symbols:delete" size="1.25rem" />
+                            </RedButton>
+                        </div>
+                    </li>
+                </ul>
 
-        </main>
+                <WhiteButton>
+                    <Icon name="carbon:task-add" size="1.5rem" class="mr-2" />
+                    <span>Criar tarefa</span>
+                </WhiteButton>
+
+            </main>
 
         </DefaultCard>
 
 
     </div>
-
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
-const { projectId } = useRoute().params;
-/*
-> const dadosProjeto = await TaskUnityApi.get(`/projects/${projectId}`);
-*/
+const projeto = ref(null);
+
+onMounted(async () => {
+    const { params } = useRoute(); // useRoute retorna a instância do Vue Router, não é uma função
+
+    // Agora, você pode acessar o parâmetro projectId diretamente
+    const projectId = params.projetoID;
+
+    try {
+        const response = await fetch(`http://localhost:8080/projects/${projectId}`);
+        const data = await response.json();
+        projeto.value = data;
+    } catch (error) {
+        console.error('Erro ao obter dados do projeto:', error);
+    }
+});
 
 definePageMeta({
     layout: 'dashboard'
-})
+});
 
 </script>

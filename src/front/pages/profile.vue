195717a4a27145c569ae6be4946a1660433c3f5a
luizfelipecm1 <142749233+luizfelipecm1@users.noreply.github.com>
2023-11-30 15:42:43 -0300

    #41 #56 [Dev] Tela Profile desenvolvida

--- src/front/pages/profile.vue
+++ src/front/pages/profile.vue
@@ -1,11 +1,40 @@
 <template>
-    Resto da página Profile.  
+    <DefaultCard>
+      <header>
+        <h2 class="text-4xl font-semibold tracking-tighter" style="padding: 20px;"> Olá, {{ user.name }}</h2>
+        <DefaultCard class="overflow-auto w-full" style="min-width: 360px; height: 360px;">
+                    <header>
+                      <hr class="my-2 linha-colorida">
+                      <h4 class="text-2xl">Nome: {{ user.name }}</h4>                     
+                        <h4 class="text-2xl">ID: {{ user.id }}</h4>
+                        <h4 class="text-2xl">Email: {{ user.email }}</h4>
+                        <h4 class="text-2xl">Função: {{ user.userFunction }}</h4>
+                        <hr class="my-2 linha-colorida">  
+                    </header>
+                </DefaultCard>
+      </header>
+      
+    </DefaultCard> 
 </template>
 
 <script setup>
+window.onload = loadUser()
 
 definePageMeta({
   layout: 'dashboard'
 })
+
+var user = ref([]);
+
+function loadUser(){
+    console.log("Load users")
+    apiFetch('/users/2')
+    .then(res => {
+        user.value = res.data;
+        console.log(user.value);
+    }).catch(err => {
+        console.log(err)
+    })
+}
 
 </script>

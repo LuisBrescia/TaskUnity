f350def852fd3d3b72329a870e749914365d874f
LuisBrescia <luisfelipebrescia@gmail.com>
2024-02-14 21:50:42 -0300

    componentizacao de grupo de blobs

--- src/front/layouts/dashboard.vue
+++ src/front/layouts/dashboard.vue
@@ -1,23 +1,5 @@
 <template>
-    <div class="blobs fixed pointer-events-none" style="height: 100vh; width: 100vw;">
-        <BlueBlob class="animate-ping right-full" style="top: 0%;" />
-        <BlueBlob class="animate-spin right-full" style="top: 0%;" />
-        <GreenBlob class="animate-ping right-full" style="top: 25%;" />
-        <GreenBlob class="animate-spin right-full" style="top: 25%;" />
-        <YellowBlob class="animate-ping right-full" style="top: 50%;" />
-        <YellowBlob class="animate-spin right-full" style="top: 50%;" />
-        <RedBlob class="animate-ping right-full" style="top: 75%;" />
-        <RedBlob class="animate-spin right-full" style="top: 75%;" />
-
-        <BlueBlob class="animate-ping left-full" style="top: 0%;" />
-        <BlueBlob class="animate-spin left-full" style="top: 0%;" />
-        <GreenBlob class="animate-ping left-full" style="top: 25%;" />
-        <GreenBlob class="animate-spin left-full" style="top: 25%;" />
-        <YellowBlob class="animate-ping left-full" style="top: 50%;" />
-        <YellowBlob class="animate-spin left-full" style="top: 50%;" />
-        <RedBlob class="animate-ping left-full" style="top: 75%;" />
-        <RedBlob class="animate-spin left-full" style="top: 75%;" />
-    </div>
+    <BlobsCollection />
     <main class="flex flex-col">
         <div class="flex-1 w-full py-3 px-5" style="height: 90svh">
             <slot />
@@ -89,10 +71,6 @@ function modalSair() {
 </script>
 
 <style scoped>
-.blobs>* {
-    width: 25vh;
-    filter: blur(100px);
-}
 
 main {
     min-height: 100vh;
